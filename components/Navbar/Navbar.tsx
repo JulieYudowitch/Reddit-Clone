84962b5d9ae06a879ffb2928f2e39a8ab13ca9eb
julie yudowitch <julieyudo@gmail.com>
2023-04-22 17:32:14 -0400

    added searchbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,19 +1,25 @@
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import SearchInput from './SearchInput';
 
 
 const Navbar:React.FC = () => {
     
     return (
-        <Flex bg="white" height="44px" padding="6px 12px">
-            <Flex align="center">
-                <Image src="images/redditFace.svg" height="30px" alt="" />
-                <Image src="/images/redditText.svg" height="46px" display={{ base: "none", md: "unset" }} alt="" />
-            </Flex>
-            {/*<Directory />
-            <SearchInput />
-    <RightContent />*/}
+      <Flex bg="white" height="44px" padding="6px 12px">
+        <Flex align="center">
+          <Image src="images/redditFace.svg" height="30px" alt="" />
+          <Image
+            src="/images/redditText.svg"
+            height="46px"
+            display={{ base: "none", md: "unset" }}
+            alt=""
+          />
         </Flex>
-    )
+        <SearchInput />
+        {/*<Directory />
+                <RightContent />*/}
+      </Flex>
+    );
 }
 export default Navbar;
--- components/Navbar/SearchInput.tsx
+++ components/Navbar/SearchInput.tsx
@@ -1,31 +1,20 @@
-import { PhoneIcon } from '@chakra-ui/icons';
+import { SearchIcon } from '@chakra-ui/icons';
 import { Flex, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react';
 import React from 'react';
+import { CheckIcon } from '@chakra-ui/icons/dist/Check';
 
 type SearchInputProps = {
-  children: React.ReactNode;
+ 
 };
 
 const SearchInput: React.FC<SearchInputProps> = () => {
     return (
-      <Flex>
+        <Flex flexGrow={1} mr={2} align="center">
         <InputGroup>
-          <InputLeftElement
-            pointerEvents="none"
-            children={<PhoneIcon color="gray.300" />}
-          />
-          <Input type="tel" placeholder="Phone number" />
-        </InputGroup>
-
-        <InputGroup>
-          <InputLeftElement
-            pointerEvents="none"
-            color="gray.300"
-            fontSize="1.2em"
-            children="$"
-          />
-          <Input placeholder="Enter amount" />
-          <InputRightElement children={<CheckIcon color="green.500" />} />
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="gray.400" mb={1} />
+          </InputLeftElement>
+          <Input placeholder="Search Reddit" fontSize='10pt' _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", border: "1px solid", borderColor: "blue.500" }} height="34px" bg="gray.50" />
         </InputGroup>
       </Flex>
     );
