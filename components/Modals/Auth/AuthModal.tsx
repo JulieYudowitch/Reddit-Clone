8abc4466f6e805ca3b996efaec36a3fe010db1eb
julie yudowitch <julieyudo@gmail.com>
2023-04-23 00:45:16 -0400

    added recoil for modals

--- components/Modals/Auth/AuthModal.tsx
+++ components/Modals/Auth/AuthModal.tsx
@@ -1,7 +1,27 @@
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react';
 import React from 'react';
+import { useRecoilState } from 'recoil'
+import { authModalState } from '@/Atoms/AuthModalAtom';
 
 const AuthModal:React.FC = () => {
-    
-    return <div>Have a good coding</div>
+    const [modalState, setModalState] = useRecoilState(authModalState);
+    const handleClose = () => {
+        setModalState(prev => ({
+            ...prev,
+            open: false,
+        }))
+    }
+    return (
+        <>
+       <Modal isOpen={modalState.open} onClose={handleClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody></ModalBody>
+          </ModalContent>
+        </Modal>
+      </>
+    );
 }
 export default AuthModal;
--- components/Navbar/RightContent/RightContent.tsx
+++ components/Navbar/RightContent/RightContent.tsx
@@ -1,6 +1,7 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import AuthButtons from './AuthButtons';
+import AuthModal from '@/components/Modals/Auth/AuthModal';
 
 type RightContentProps = {
     
@@ -10,7 +11,7 @@ const RightContent:React.FC<RightContentProps> = () => {
     
     return (
         <>
-            {/*<AuthModal />*/}
+            <AuthModal />
             <Flex justify="center" align="center">
                 <AuthButtons />
             </Flex>
